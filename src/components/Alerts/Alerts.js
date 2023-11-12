895add822a31467659a4c04238b47aac95359106
Air128731 <henry.scott0x8080@gmail.com>
2023-11-12 08:23:51 -0500

    icon and button

--- src/components/Alerts/Alerts.js
+++ src/components/Alerts/Alerts.js
@@ -2,7 +2,7 @@ import CustomAlert from './CustomAlert';
 
 export default function Alerts () {
     return (
-      <div className="bg-[#17191B] p-10 rounded-xl flex flex-col gap-3">
+      <div className="bg-[#17191B] p-0 lg:p-10 rounded-xl flex flex-col gap-3 overflow-hidden">
         <p className='font-medium text-white'>With icon</p>
         <CustomAlert type="1" alertType="home" title="An example alert with an icon" subtitle="" />
         <CustomAlert type="1" alertType="success" title="An example alert with an icon" subtitle="" />
@@ -13,7 +13,7 @@ export default function Alerts () {
         <CustomAlert type="2" alertType="success" title="An example alert with an icon" subtitle="" />
         <CustomAlert type="2" alertType="error" title="An example alert with an icon" subtitle="" />
         <CustomAlert type="2" alertType="warning" title="An example alert with an icon" subtitle="" />
-        <p className='mt-10 font-medium text-white'>With icon and cross</p>
+        <p className='mt-10 font-medium text-white'>With icon and button</p>
         <CustomAlert type="3" alertType="home" title="An example alert with an icon" subtitle="An example alert with an icon and button" />
         <CustomAlert type="3" alertType="success" title="An example alert with an icon" subtitle="An example alert with an icon and button" />
         <CustomAlert type="3" alertType="error" title="An example alert with an icon" subtitle="An example alert with an icon and button" />
